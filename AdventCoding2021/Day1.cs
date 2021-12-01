84d3bf93cc6d8aebc33999d9422f5d5e17ed90a0
Andrew Mumby <andrew@mumby.info>
2021-12-01 10:02:31 +0000

    Added Day 1

--- AdventCoding2021/Day1.cs
+++ AdventCoding2021/Day1.cs
@@ -8,5 +8,43 @@ namespace AdventCoding2021
 {
     internal class Day1
     {
+        private static List<int> ParseInput(string input)
+        {
+            string[] inputList = input.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+            List<int> numberList = new List<int>();
+            foreach (string inputItem in inputList)
+            {
+                numberList.Add(Int32.Parse(inputItem));
+            }
+            return numberList;
+        }
+        public static string A(string input)
+        {
+            List<int> numbers = ParseInput(input);
+            int count = 0;
+            for (int i = 0; i < numbers.Count - 1; i++)
+            {
+                if (numbers[i] < numbers[i + 1])
+                {
+                    count++;
+                }
+            }
+            return count.ToString();
+        }
+
+        public static string B(string input)
+        {
+            List<int> numbers = ParseInput(input);
+            int count = 0;
+            for (int i = 0; i < numbers.Count - 3; i++)
+            {
+                if (numbers[i] < numbers[i + 3])
+                {
+                    count++;
+                }
+
+            }
+            return count.ToString();
+        }
     }
 }
